caec2ddc39a02e469664aeae658ebe764804158a
Sandhyaaadpa72 <adapasandhya732@gmail.com>
2025-07-14 17:46:10 +0530

    HYPERPARAMETRE TUNING

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -19,18 +19,25 @@ from src.logger import logging
 
 from src.utils import save_object, evaluate_models
 
+
 @dataclass
 class ModelTrainerConfig:
-    trained_model_file_path=os.path.join("artifacts","model.pkl")
+    trained_model_file_path = os.path.join("artifacts", "model.pkl")
+
 
 class ModelTrainer:
     def __init__(self):
-        self.model_trainer_config=ModelTrainerConfig()
+        self.model_trainer_config = ModelTrainerConfig()
 
-    def initiate_model_trainer(self,train_array,test_array):
+    def initiate_model_trainer(self, train_array, test_array):
         try:
             logging.info("Split training and test input data")
-            X_train,y_train,X_test,y_test=(train_array[:,:-1],train_array[:,-1],test_array[:,:-1],test_array[:,-1])
+            X_train, y_train, X_test, y_test = (
+                train_array[:, :-1],
+                train_array[:, -1],
+                test_array[:, :-1],
+                test_array[:, -1]
+            )
 
             models = {
                 "Random Forest": RandomForestRegressor(),
@@ -42,21 +49,40 @@ class ModelTrainer:
                 "AdaBoost Regressor": AdaBoostRegressor(),
             }
 
-            # ✅ Fix: Define params
             params = {
-                "Random Forest": {},
-                "Decision Tree": {},
-                "Gradient Boosting": {},
+                "Decision Tree": {
+                    'criterion': ['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                },
+                "Random Forest": {
+                    'n_estimators': [8, 16, 32, 64, 128, 256]
+                },
+                "Gradient Boosting": {
+                    'learning_rate': [0.1, 0.01, 0.05, 0.001],
+                    'subsample': [0.6, 0.7, 0.75, 0.8, 0.85, 0.9],
+                    'n_estimators': [8, 16, 32, 64, 128, 256]
+                },
                 "Linear Regression": {},
-                "XGBRegressor": {},
-                "CatBoosting Regressor": {},
-                "AdaBoost Regressor": {},
+                "XGBRegressor": {
+                    'learning_rate': [0.1, 0.01, 0.05, 0.001],
+                    'n_estimators': [8, 16, 32, 64, 128, 256]
+                },
+                "CatBoosting Regressor": {
+                    'depth': [6, 8, 10],
+                    'learning_rate': [0.01, 0.05, 0.1],
+                    'iterations': [30, 50, 100]
+                },
+                "AdaBoost Regressor": {
+                    'learning_rate': [0.1, 0.01, 0.5, 0.001],
+                    'n_estimators': [8, 16, 32, 64, 128, 256]
+                }
             }
 
             model_report = evaluate_models(X_train, y_train, X_test, y_test, models, params)
 
             best_model_score = max(sorted(model_report.values()))
-            best_model_name = list(model_report.keys())[list(model_report.values()).index(best_model_score)]
+            best_model_name = list(model_report.keys())[
+                list(model_report.values()).index(best_model_score)
+            ]
             best_model = models[best_model_name]
 
             if best_model_score < 0.6:
@@ -74,4 +100,4 @@ class ModelTrainer:
             return r2_square
 
         except Exception as e:
-            raise CustomException(e,sys)
+            raise CustomException(e, sys)
